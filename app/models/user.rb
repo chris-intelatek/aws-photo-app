b722146a4525ee44cdc95c54bccc181afd753c37
Chris Cunningham <chris@intelatek.com>
2017-09-04 22:17:50 +0000

    Add payment fields to signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   has_one :payment
-  accept_nested_attributes_for :payment
+  accepts_nested_attributes_for :payment
 end
